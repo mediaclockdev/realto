f6979ab3a90155f09324136ac8ae30eb81bd7702
Harsh Mehta <mediaclockdev@gmail.com>
2026-05-07 13:16:20 +0530

    Update PropertyListingCard.tsx

--- components/PropertyListing/PropertyListingCard.tsx
+++ components/PropertyListing/PropertyListingCard.tsx
@@ -270,7 +270,7 @@ const PropertyListingCard: React.FC<PropertyListingCardProps> = ({
                     </p>
                   </div>
                 </div>
-                <div className="flex items-center justify-between gap-2 ">
+                <div className="flex items-center gap-24 ">
                   <div className="flex items-center gap-2 min-w-0">
                     <Image
                       src={rentmoney}
